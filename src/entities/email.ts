d0554a23f5912976ea0e670e157141daded2aff1
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-08-09 21:35:11 -0300

    refactor: chand import invalid email class

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,5 +1,5 @@
 import { Either, right, left } from '../shared/either'
-import { InvalidEmailError } from './invalid-email-error'
+import { InvalidEmailError } from './invalid.email.error'
 
 export class Email {
   private readonly email: string
