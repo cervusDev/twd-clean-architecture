a6434f1b3f6f2be292afdcfb163cd090135c9964
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-08-09 21:36:09 -0300

    feat: implement name validation on user class

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,10 +1,17 @@
 import { Either, left } from '../shared/either'
 import { Email } from './email'
-import { InvalidEmailError } from './invalid-email-error'
+import { InvalidEmailError } from './invalid.email.error'
+import { InvalidNameError } from './invalid.name.error'
+import { Name } from './name'
 import { UserData } from './user-data'
 
 export class User {
   static create (user: UserData): Either<InvalidEmailError, User> {
+    const validName = Name.create(user.name)
+    if (validName.isLeft()) {
+      return left(new InvalidNameError())
+    }
+
     const validEmail = Email.create(user.email)
     if (validEmail.isLeft()) {
       return left(new InvalidEmailError())
