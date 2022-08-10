ed6c4a42275d530da85bc13532fce27d119963b3
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-08-09 21:36:36 -0300

    test: should not create user with invalid name (too few chars)

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from '../shared/either'
-import { InvalidEmailError } from './invalid-email-error'
+import { InvalidEmailError } from './invalid.email.error'
+import { InvalidNameError } from './invalid.name.error'
 import { User } from './user'
 
 describe('User domain entity', () => {
@@ -7,5 +8,11 @@ describe('User domain entity', () => {
     const invalidEmail = 'invalid_email'
     const error = User.create({ name: 'any name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  it('should not create user with invalid name (too few chars)', () => {
+    const invalidName = 'O    '
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
