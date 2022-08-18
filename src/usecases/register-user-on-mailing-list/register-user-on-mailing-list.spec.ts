26ad2055a157078b8ac4be7491be92befdee99db
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-08-17 21:14:21 -0300

    test: add a valid user on mainling list

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -4,13 +4,14 @@ import { InMemoryUserRepository } from '../repository/user.inmemory.repository'
 import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
 
 describe('Register user on mailing list', () => {
-  it('should add user with complete data to mailing list', () => {
-    const user: UserData[] = []
-    console.log(user)
-    const repo: UserRepoistory = new InMemoryUserRepository(user)
+  it('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    const repo: UserRepoistory = new InMemoryUserRepository(users)
     const usecsae: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const json = { name: 'any_name', email: 'any@mail.com' }
     const response = await usecsae.RegisterUserOnMailingList(json)
-    expect((await user).name).toBe(json.name)
+    const user = repo.findUserByEmail(json.email)
+    expect((await user).name).toBe('any_name')
+    expect(response.value.name).toBe('any_name')
   })
 })
